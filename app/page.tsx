d72094e2b8cb6cb58b31e17cc1de8c9dff25d607
hakeem-jc <hakeemclarke14@gmail.com>
2024-07-20 13:06:27 -0500

    feat: Add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Header from "./components/Header";
+import Map from "./components/Map";
+
 export default function Home() {
   return (
     <main>
-      <h1>Placeholder</h1>
+      <Header />
+      <Map />
     </main>
   );
 }
