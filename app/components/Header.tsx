659224c62248681381ca03e4be75b017748ac289
hakeem-jc <hakeemclarke14@gmail.com>
2024-07-21 11:06:28 -0500

    feat: Add modal

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,4 @@
-const Header = () => {
+const Header = ( { setOpenModal } : { setOpenModal: () => void }) => {
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -10,6 +10,7 @@ const Header = () => {
         <div className="flex space-x-3 md:space-x-0">
           <button
             type="button"
+            onClick={() => setOpenModal()}
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Choose Locations
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+'use client'
+import { useState } from "react";
 import Header from "./components/Header";
 import Map from "./components/Map";
+import Modal from "./components/Modal";
 
 export default function Home() {
+  const [openModal, setOpenModal] = useState(false);
+
+  const toggleModal = () => setOpenModal(!openModal)
+
   return (
-    <main className="h-screen w-full">
-      <Header />
+    <main className="flex flex-col h-screen w-full">
+      {openModal && <Modal />}
+      <Header setOpenModal={toggleModal}/>
       <Map />
     </main>
   );
