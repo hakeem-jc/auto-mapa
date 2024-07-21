0a1048771edf2aa93800e8aad53826d02b69835c
hakeem-jc <hakeemclarke14@gmail.com>
2024-07-21 11:28:57 -0500

    feature: Center modal

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -3,7 +3,6 @@ const Modal = () => {
     <div className="absolute flex flex-col h-screen w-full z-10 justify-center items-center bg-black/50">
       <div
         aria-hidden="true"
-        className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
       >
         <div className="p-4 w-full max-w-md max-h-full mx-auto">
           <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
