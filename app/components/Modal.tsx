184017d35214e3614f63db0fd4991742844f4d02
Hakeem Clarke <hakeem.clarke@speurgroup.com>
2024-10-16 14:29:41 +0200

    TODO: Reminder to update modal

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+// TODO - Convert Modal into list of unmapped locations
 const Modal = ({ toggleModal }: { toggleModal: () => void }) => {
   const [url, setUrl] = useState<string>("");
 
